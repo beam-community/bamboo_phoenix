66ac363051123f925f63ad361b51679afd303265
German Velasco <germsvel@gmail.com>
2021-02-18 13:40:13 -0500

    Bring Bamboo.Phoenix from Bamboo library
    What changed?
    =============
    
    This is the first commit of a new library `bamboo_phoenix` to extract
    the `Bamboo.Phoenix` module out of `Bamboo`.
    
    Those who want to use `Phoenix` for rendering emails can do so by
    including `bamboo_phoenix` in their applications. Those who prefer using
    `Bamboo`'s built-in rendering `View`/`Template` modules can use those
    instead.

---
+++ README.md
@@ -0,0 +1,21 @@
+# BambooPhoenix
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `bamboo_phoenix` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:bamboo_phoenix, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/bamboo_phoenix](https://hexdocs.pm/bamboo_phoenix).
+
---
+++ mix.exs
@@ -0,0 +1,45 @@
+defmodule BambooPhoenix.MixProject do
+  use Mix.Project
+
+  @project_url "https://github.com/thoughtbot/bamboo_phoenix"
+  @version "1.0.0"
+
+  def project do
+    [
+      app: :bamboo_phoenix,
+      version: @version,
+      elixir: "~> 1.8",
+      source_url: @project_url,
+      homepage_url: @project_url,
+      start_permanent: Mix.env() == :prod,
+      description: "Bamboo library to use Phoenix for rendering emails",
+      package: package(),
+      docs: [main: "readme", extras: ["README.md"]],
+      deps: deps()
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["German Velasco"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @project_url}
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:bamboo, "~> 1.7.0"},
+      {:phoenix, "~> 1.3.0"},
+      {:phoenix_html, "~> 2.2", only: :test}
+    ]
+  end
+end
